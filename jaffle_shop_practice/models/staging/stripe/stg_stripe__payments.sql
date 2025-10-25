b1f06958e4a7a450f6098ece6e7d95e317f6075c
MyoThet <mr.myothet@gmail.com>
2025-10-25 17:24:38 +0700

    fix the errors

--- jaffle_shop_practice/models/staging/stripe/stg_stripe__payments.sql
+++ jaffle_shop_practice/models/staging/stripe/stg_stripe__payments.sql
@@ -1,6 +1,9 @@
 select
     id as payment_id,
-    order_id,
-    payment_method,
-    amount
-from raw.stripe.payments
+    order_id as order_id,
+    payment_method as payment_method,
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount
+
+from {{ ref('raw_payments') }}
