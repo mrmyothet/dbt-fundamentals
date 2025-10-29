d1b5815e69417e4b857d70c349ed0bdae1d3cf12
MyoThet <mr.myothet@gmail.com>
2025-10-30 00:59:08 +0700

    add a snapshot

---
+++ nyc_yellow_taxi/snapshots/vehicle_list_snapshot.sql
@@ -0,0 +1,13 @@
+{% snapshot vehicle_list_snapshot %}
+
+{{
+    config(
+      target_schema='main',
+      unique_key='license_id',
+      strategy='timestamp',
+      updated_at='last_updated'
+    )
+}}
+
+select * from raw.vehicle_list
+{% endsnapshot %}
