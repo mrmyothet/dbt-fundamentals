f0296c34ba4365dcaefb86fb37d728184e288c19
MyoThet <mr.myothet@gmail.com>
2025-11-01 16:37:40 +0700

    jinja - for loop and case when

---
+++ jaffle_shop_practice/models/intermediate/int_orders_pivoted.sql
@@ -0,0 +1,28 @@
+with payments as (
+    select *
+    from {{ ref('stg_payments') }}
+    --where status = 'success'
+),
+
+pivoted as (
+    select
+        order_id,
+        {% set payment_methods = ['bank_transfer', 'credit_card', 'coupon', 'gift_card'] %}
+
+        {% for payment_method in payment_methods %}
+            sum(
+                case
+                    when
+                        payment_method = '{{ payment_method }}'
+                        then amount else
+                        0
+                end
+            ) as {{ payment_method }}_amount,
+        {% endfor %}
+
+    from payments
+    group by 1
+)
+
+select *
+from pivoted
