01e2eb3487459ef69d55a1c8d68eafc8e0f9270c
MyoThet <mr.myothet@gmail.com>
2025-11-09 15:31:27 +0700

    In CASE of rivalry - barcelona vs real madrid

---
+++ european_soccer/models/spain/Barcelona_vs_RealMadrid.sql
@@ -0,0 +1,12 @@
+SELECT 
+	date,
+	CASE WHEN hometeam_id = 8634 THEN 'FC Barcelona' 
+         ELSE 'Real Madrid CF' END as home,
+	CASE WHEN awayteam_id = 8634 THEN 'FC Barcelona' 
+         ELSE 'Real Madrid CF' END as away,
+	CASE WHEN home_goal > away_goal AND hometeam_id = 8634 THEN 'Barcelona win!'
+        WHEN home_goal < away_goal AND awayteam_id = 8633 THEN 'Real Madrid win!'
+        ELSE 'Tie!' 
+	END AS outcome
+FROM matches_spain
+WHERE hometeam_id = 8634 AND awayteam_id = 8633;
